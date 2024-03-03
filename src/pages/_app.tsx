964968b5957016b87ecf68eaae92d76d0e528a28
hye-yeong <ghdwls69@naver.com>
2024-03-03 19:37:22 +0900

    eslint, prettier 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+//글로벌 스타일
+const GlobalStyle = createGlobalStyle`
+html,
+body,
+textarea{
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica neue, sans-serif;
 }
+
+* {
+  box-sizing: border-box;
+}
+`
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
