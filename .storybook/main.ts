9d41263afc7b5e06d85489f55e127bdcddd5b8ab
hye-yeong <ghdwls69@naver.com>
2024-03-03 21:46:20 +0900

    애드온 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,8 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin" 
+import path from "path"
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -7,7 +10,8 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-themes"
+    "@storybook/addon-themes",
+    "@storybook/addon-postcss"
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -16,6 +20,23 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-  staticDirs: ['public']
+  staticDirs: ['public'],
+  // babel: async options => ({
+  //   ...options,
+  //   plugins: [
+  //     "babel/plugin-proposal-class-properties",
+  //     "babel/plugin-proposal-private-methods",
+  //     "babel/plugin-proposal-private-property-in-object"
+  //   ]
+  // }),
+
+  // webpackFinal: async (config) => {
+  //   config.resolve.plugins = [
+  //     new TsconfigPathsPlugin({
+  //       configFile: path.resolve(__dirname, '../tsconfig.json')
+  //     })
+  //   ]
+  //   return config
+  // }
 };
 export default config;
